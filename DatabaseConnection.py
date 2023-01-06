edd5793d3f5721e47f051f2ba35c32e010b90a64
Kelvin munene <43162440+munene8@users.noreply.github.com>
2023-01-06 16:30:38 +0300

    Update DatabaseConnection.py
    Signed-off-by: Kelvin munene <43162440+munene8@users.noreply.github.com>

--- DatabaseConnection.py
+++ DatabaseConnection.py
@@ -2,9 +2,18 @@ import mysql.connector
 
 mydb = mysql.connector.connect ( host = "local host", user = "Munene", password = "Kel7808mun")
 mycursor = mydb.cursor()
-mycursor.execute("CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, FNAME VARCHAR(255),Lname VARCHAR(255), Location VARCHAR(255), destination VARCHAR (255)"
-mycursor.execute( " INSERT INTO Customers VALUE ( "Kelvin", "Munene", "Karatina", "Nairobi" )
-mycursor.execute ( "ALTER TABLE Customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY")
+connection.execute(''' CREATE TABLE Customers
+         (Cust_Id INT PRIMARY KEY     NOT NULL,
+         FNAME           TEXT    NOT NULL,
+         LNAME           INT     NOT NULL,
+         Destination       INT);
+         ''')
+#populating the database 
+mycursor = connect.execute(INSERT INTO Customers Value( "kelvin", "munene" "Nairobi"))
+#function for retrieving a record from the database
+def Retrieve_a_record():
+    x = connect.execute(SELECT FROM Customers WHERE Name= "kelvin")
+    print(x)
 
 
          
